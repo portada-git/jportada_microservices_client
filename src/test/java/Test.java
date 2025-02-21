66d568aebcea7bb7a9347944d95ff0ce1880a417
jcanell4 <jcanell4@xtec.cat>
2025-02-21 02:00:12 +0100

    Changes in extracted data and in csv and json formater to support non string values

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -14,14 +14,14 @@ import org.elsquatrecaps.portada.jportadamicroservice.client.services.imagefile.
 public class Test {
     
     public static void main(String[] args) {
-//        JPortadaMicroservice prg = new JPortadaMicroservice();
+        JPortadaMicroservice prg = new JPortadaMicroservice();
 //        String[] args2 = {"requestAccess", "-tm", "bcn", "-m", "josep.canellas@gmail.com", "-f"};
 //        String[] args2 = {"verifyCode", "-tm", "bcn", "-m", "josep.canellas@gmail.com", "-c", "dPPvzUcB"};
 //        String[] args2 = {"ocrAll", "-tm", "bcn", "-i", "corregides2", "-o", "ocr4"};
 //        String[] args2 = {"reorderAll", "-tm", "bcn", "-i", "corregides2", "-o", "ordered"};
 //        String[] args2 = {"pytest", "-tm", "bcn"};
 //        String[] args2 = {"jtest", "-tm", "bcn"};
-//        String[] args2 = {"ocrJsonAll", "-tm", "bcn", "-i", "corregides2", "-o", "ocr_json"};
+        String[] args2 = {"ocrJsonAll", "-tm", "bcn", "-i", "corregides2", "-o", "ocr_json"};
 //        String[] args2 = {"accept_key", "-tm", "bcn", "-k", "ogtoledano__AT_SIGN__gmail.com_Cg1Z_M6G_public.pem", "-u", "??", "-p", "???"};
 //        String[] args2 = {
 //            "extract", 
@@ -31,18 +31,18 @@ public class Test {
 //            "--extractConfigMode", "R", 
 //            "--extractConfigProtertiesFile", "db"
 //        };
-//        prg.run(args2);
+        prg.run(args2);
 
-        Configuration config = new Configuration();
-        config.configure();
-        Map<String, ConnectionMs> conDataList = new HashMap<>();
-        for(String ctx: PublisherService.msContext){
-            conDataList.put(ctx, new ConnectionMs(config.getProtocols(ctx), config.getPort(ctx), config.getHosts(ctx), config.getPrefs(ctx)));
-        }
-        ImageQualityFilterService serv = ImageQualityFilterService.getInstance().init(conDataList);
-        System.out.println(serv.processMulticlassFilter("data/imatgeTransparencia.jpg").toString());
-        System.out.println(serv.processBinaryFilter("data/imatgeTransparencia.jpg", 0.2).toString());
-        System.out.println(serv.processBinaryFilter("data/imatgeTransparencia.jpg").toString());
+//        Configuration config = new Configuration();
+//        config.configure();
+//        Map<String, ConnectionMs> conDataList = new HashMap<>();
+//        for(String ctx: PublisherService.msContext){
+//            conDataList.put(ctx, new ConnectionMs(config.getProtocols(ctx), config.getPort(ctx), config.getHosts(ctx), config.getPrefs(ctx)));
+//        }
+//        ImageQualityFilterService serv = ImageQualityFilterService.getInstance().init(conDataList);
+//        System.out.println(serv.processMulticlassFilter("data/imatgeTransparencia.jpg").toString());
+//        System.out.println(serv.processBinaryFilter("data/imatgeTransparencia.jpg", 0.2).toString());
+//        System.out.println(serv.processBinaryFilter("data/imatgeTransparencia.jpg").toString());
     }
      
     
